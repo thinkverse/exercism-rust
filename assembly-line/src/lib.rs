ecf0ad3bd015fbdbe12c55b807943f33ba9e5acd
Kim Hallberg <hallberg.kim@gmail.com>
2023-01-06 12:47:21 +0100

    Complete `Assembly Line` exercise

--- assembly-line/src/lib.rs
+++ assembly-line/src/lib.rs
@@ -3,9 +3,14 @@
 #![allow(unused)]
 
 pub fn production_rate_per_hour(speed: u8) -> f64 {
-    unimplemented!("calculate hourly production rate at speed: {}", speed)
+    return (speed as f64 * 221.0) * match speed {
+        1..=4 => 1.0,
+        5..=8 => 0.9,
+        9|10 => 0.77,
+        _ => 0.0
+    };
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
-    unimplemented!("calculate the amount of working items at speed: {}", speed)
+    (production_rate_per_hour(speed) / 60.0).floor() as u32
 }

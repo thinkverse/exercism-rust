50141a9136bccfdbaa3c34969775274bc4760e5a
Kim Hallberg <hallberg.kim@gmail.com>
2023-01-11 04:32:38 +0100

    Complete `Semi Structured Logs` exercise

--- semi-structured-logs/src/lib.rs
+++ semi-structured-logs/src/lib.rs
@@ -11,14 +11,20 @@ pub enum LogLevel {
 }
 /// primary function for emitting logs
 pub fn log(level: LogLevel, message: &str) -> String {
-    unimplemented!("return a message for the given log level")
+    let str = match level {
+        LogLevel::Info => "INFO",
+        LogLevel::Warning => "WARNING",
+        LogLevel::Error => "ERROR",
+    };
+
+    format!("[{}]: {}", str, message)
 }
 pub fn info(message: &str) -> String {
-    unimplemented!("return a message for info log level")
+    log(LogLevel::Info, message)
 }
 pub fn warn(message: &str) -> String {
-    unimplemented!("return a message for warn log level")
+    log(LogLevel::Warning, message)
 }
 pub fn error(message: &str) -> String {
-    unimplemented!("return a message for error log level")
+    log(LogLevel::Error, message)
 }
